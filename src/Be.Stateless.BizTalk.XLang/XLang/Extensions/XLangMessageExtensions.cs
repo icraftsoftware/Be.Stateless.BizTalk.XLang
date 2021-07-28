33d6c92d319fb1a49bde6cb4ce7be4f433835818
François Chabot <francois.chabot@stateless.be>
2021-07-28 12:44:41 +0200

    Bump up NuGets
    Catch block & C# 9 Rewrites
    Refresh git submodules

--- src/Be.Stateless.BizTalk.XLang/XLang/Extensions/XLangMessageExtensions.cs
+++ src/Be.Stateless.BizTalk.XLang/XLang/Extensions/XLangMessageExtensions.cs
@@ -1,6 +1,6 @@
 ﻿#region Copyright & License
 
-// Copyright © 2012 - 2020 François Chabot
+// Copyright © 2012 - 2021 François Chabot
 // 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -64,10 +64,7 @@ namespace BizTalk.Factory.XLang.Extensions
 
 				if (xm != null) contextProperties = xm.GetContextProperties();
 			}
-			catch (Exception exception)
-			{
-				if (exception.IsFatal()) throw;
-			}
+			catch (Exception exception) when (!exception.IsFatal()) { }
 
 			return new XmlQNameTable(contextProperties ?? new Hashtable());
 		}
